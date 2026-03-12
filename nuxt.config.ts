ec6598c4c1110d06dbb1596866026707c4935ac3
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2026-03-12 10:12:38 -0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   css: ["~/assets/css/main.css"],
   modules: ["@nuxt/ui"],
-
+  nitro: {
+    preset: "cloudflare_module",
+  },
   devtools: { enabled: true },
 });
